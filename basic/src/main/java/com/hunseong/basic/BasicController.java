4dde70177d42e91ba23963088abbfcd022c22338
Hunseong <phunseong@gmail.com>
2022-05-10 18:35:04 +0900

    each

--- basic/src/main/java/com/hunseong/basic/BasicController.java
+++ basic/src/main/java/com/hunseong/basic/BasicController.java
@@ -82,6 +82,12 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
     @Data
     static class User {
         private String username;
@@ -91,5 +97,13 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
     }
 }
