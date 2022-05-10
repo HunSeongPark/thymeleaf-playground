68c80f83d0ff033a0491a30653cd3044c478027d
Hunseong <phunseong@gmail.com>
2022-05-10 17:47:01 +0900

    link - basic, query parameter, path variable

--- basic/src/main/java/com/hunseong/basic/BasicController.java
+++ basic/src/main/java/com/hunseong/basic/BasicController.java
@@ -57,6 +57,13 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Data
     static class User {
         private String username;
